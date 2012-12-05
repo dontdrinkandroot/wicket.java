fba95d7ca62f7b9dacc8ca1a0619d97e6e6f933f
Philip Washington Sorst <github@sorst.net>
2012-12-05 01:24:39 +0100

    Making ScrollToBottomBehavior inactive white loading

--- core/src/main/java/net/dontdrinkandroot/wicket/behavior/ajax/ScrollToBottomBehavior.java
+++ core/src/main/java/net/dontdrinkandroot/wicket/behavior/ajax/ScrollToBottomBehavior.java
@@ -4,14 +4,18 @@ import org.apache.wicket.Component;
 import org.apache.wicket.Page;
 import org.apache.wicket.WicketRuntimeException;
 import org.apache.wicket.ajax.AbstractDefaultAjaxBehavior;
+import org.apache.wicket.ajax.attributes.AjaxCallListener;
+import org.apache.wicket.ajax.attributes.AjaxRequestAttributes;
 import org.apache.wicket.markup.head.IHeaderResponse;
-import org.apache.wicket.markup.head.OnDomReadyHeaderItem;
+import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 
 
 //TODO: This works only for pages at the moment
 public abstract class ScrollToBottomBehavior extends AbstractDefaultAjaxBehavior {
 
 	private final int offset;
+
+	private static final String ACTIVE_PREFIX = "scollToBottomActive";
 
 
 	public ScrollToBottomBehavior(int offset) {
@@ -33,22 +37,43 @@ public abstract class ScrollToBottomBehavior extends AbstractDefaultAjaxBehavior
 		}
 
 		StringBuffer scriptBuffer = new StringBuffer();
-		// scriptBuffer.append(String.format("console.log($(%s));", componentSelector));
+		scriptBuffer.append(String.format("var %s = true;", this.getActiveVarName(component)));
 		scriptBuffer.append(String.format("$(%s).scroll(function () {", componentSelector));
-		// scriptBuffer.append(String.format(
-		// "console.log('scrollTop: ' + $(%s).scrollTop(), 'documentHeight: ' + $(document).height(), 'windowHeight: ' + $(%s).height());",
-		// componentSelector,
-		// componentSelector));
 		scriptBuffer.append(String.format(
-				"if ($(%s).scrollTop() >= $(document).height() - $(%s).height() - %d) {",
+				"if (%s && $(%s).scrollTop() >= $(document).height() - $(%s).height() - %d) {",
+				this.getActiveVarName(component),
 				componentSelector,
 				componentSelector,
 				this.offset));
+		scriptBuffer.append(String.format("%s = false;", this.getActiveVarName(component)));
 		scriptBuffer.append(this.getCallbackScript());
 		scriptBuffer.append("}");
 		scriptBuffer.append("});");
 
-		response.render(new OnDomReadyHeaderItem(scriptBuffer));
+		response.render(JavaScriptHeaderItem.forScript(scriptBuffer, "scrollToBottom"));
+	}
+
+
+	@Override
+	protected void updateAjaxAttributes(AjaxRequestAttributes attributes) {
+
+		super.updateAjaxAttributes(attributes);
+		attributes.getAjaxCallListeners().add(new AjaxCallListener() {
+
+			@Override
+			public CharSequence getSuccessHandler(Component component) {
+
+				return String.format(
+						"%s = true;",
+						ScrollToBottomBehavior.this.getActiveVarName(ScrollToBottomBehavior.this.getComponent()));
+			}
+		});
+	}
+
+
+	private String getActiveVarName(Component component) {
+
+		return ScrollToBottomBehavior.ACTIVE_PREFIX + "_" + component.getPath();
 	}
 
 
