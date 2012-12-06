b4685b40eb3d50e84f073f8df60a4cdd329684ad
Philip Washington Sorst <github@sorst.net>
2012-12-06 13:30:49 +0100

    Moving wicket resources to src/main/java

--- core/src/main/java/net/dontdrinkandroot/wicket/behavior/ajax/AbstractOnClickBehavior.java
+++ core/src/main/java/net/dontdrinkandroot/wicket/behavior/ajax/AbstractOnClickBehavior.java
@@ -1,7 +1,6 @@
 package net.dontdrinkandroot.wicket.behavior.ajax;
 
 import org.apache.wicket.ajax.AjaxEventBehavior;
-import org.apache.wicket.ajax.AjaxRequestTarget;
 
 
 public abstract class AbstractOnClickBehavior extends AjaxEventBehavior {
