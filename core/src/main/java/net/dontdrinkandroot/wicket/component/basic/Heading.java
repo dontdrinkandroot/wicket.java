d56e440bb7d034f6e473b6806e88064fe6304187
Philip Washington Sorst <philip@sorst.net>
2013-10-11 16:23:13 +0200

    Adding Heading Component

---
+++ core/src/main/java/net/dontdrinkandroot/wicket/component/basic/Heading.java
@@ -0,0 +1,60 @@
+package net.dontdrinkandroot.wicket.component.basic;
+
+import java.io.Serializable;
+
+import org.apache.wicket.markup.ComponentTag;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+
+
+public class Heading extends Label {
+
+	public enum Level {
+		H1,
+		H2,
+		H3,
+		H4,
+		H5,
+		H6;
+	}
+
+
+	private final Level level;
+
+
+	public Heading(final String id, Level level) {
+
+		super(id);
+		this.level = level;
+	}
+
+
+	public Heading(final String id, Level level, Serializable label) {
+
+		super(id, Model.of(label));
+		this.level = level;
+	}
+
+
+	public Heading(final String id, Level level, IModel<?> model) {
+
+		super(id, model);
+		this.level = level;
+	}
+
+
+	public Level getLevel() {
+
+		return this.level;
+	}
+
+
+	@Override
+	protected void onComponentTag(ComponentTag tag) {
+
+		tag.setName(this.level.name().toLowerCase());
+		super.onComponentTag(tag);
+	}
+
+}
