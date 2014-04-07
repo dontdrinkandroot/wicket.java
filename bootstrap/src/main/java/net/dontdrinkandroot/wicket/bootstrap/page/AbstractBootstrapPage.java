40ead1a198ac492c8296fe6ec8778c66f197afd5
Philip Washington Sorst <philip@sorst.net>
2014-04-07 17:08:51 +0200

    Updating license information, removing bootstrap resources, referencing by cdn instead

--- bootstrap/src/main/java/net/dontdrinkandroot/wicket/bootstrap/page/AbstractBootstrapPage.java
+++ bootstrap/src/main/java/net/dontdrinkandroot/wicket/bootstrap/page/AbstractBootstrapPage.java
@@ -1,12 +1,26 @@
+/**
+ * Copyright (C) 2012, 2013 Philip W. Sorst <philip@sorst.net>
+ * and individual contributors as indicated
+ * by the @authors tag.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.dontdrinkandroot.wicket.bootstrap.page;
 
-import net.dontdrinkandroot.wicket.bootstrap.css.BootstrapCssResourceReference;
-import net.dontdrinkandroot.wicket.bootstrap.javascript.BootstrapJsResourceReference;
+import net.dontdrinkandroot.wicket.bootstrap.headeritem.BootstrapCssHeaderItem;
+import net.dontdrinkandroot.wicket.bootstrap.headeritem.BootstrapJsHeaderItem;
 
-import org.apache.wicket.markup.head.CssHeaderItem;
 import org.apache.wicket.markup.head.IHeaderResponse;
-import org.apache.wicket.markup.head.JavaScriptHeaderItem;
-import org.apache.wicket.markup.head.JavaScriptReferenceHeaderItem;
 import org.apache.wicket.markup.html.GenericWebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.IModel;
@@ -49,14 +63,11 @@ public abstract class AbstractBootstrapPage<T> extends GenericWebPage<T> {
 
 		if (this.includeBootstrapJavaScript()) {
 
-			JavaScriptReferenceHeaderItem bootstrapJsItem =
-					JavaScriptHeaderItem.forReference(new BootstrapJsResourceReference(), "bootstrap");
-
-			response.render(bootstrapJsItem);
+			response.render(new BootstrapJsHeaderItem(false));
 		}
 
 		if (this.includeBoostrapCss()) {
-			response.render(CssHeaderItem.forReference(new BootstrapCssResourceReference()));
+			response.render(new BootstrapCssHeaderItem());
 		}
 	}
 
--- experimental/src/main/java/net/dontdrinkandroot/wicket/component/jqueryui/jquery.ui.selectmenu.js
+++ experimental/src/main/java/net/dontdrinkandroot/wicket/component/jqueryui/jquery.ui.selectmenu.js
@@ -1,14 +1,20 @@
- /*
- * jQuery UI selectmenu version 1.1.0
+/*
+ * Copyright (C) 2012, 2013 Philip W. Sorst <philip@sorst.net>
+ * and individual contributors as indicated
+ * by the @authors tag.
  *
- * Copyright (c) 2009 AUTHORS.txt (http://jqueryui.com/about)
- * Dual licensed under the MIT (MIT-LICENSE.txt)
- * and GPL (GPL-LICENSE.txt) licenses.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * http://docs.jquery.com/UI
- * https://github.com/fnagel/jquery-ui/wiki/Selectmenu
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 (function($) {
 
 $.widget("ui.selectmenu", {
