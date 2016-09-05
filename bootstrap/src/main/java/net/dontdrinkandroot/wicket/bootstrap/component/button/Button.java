64773d95cbfc9db6d694f153d97d93a2c70f8e43
Philip Washington Sorst <philip@sorst.net>
2016-09-05 02:14:13 +0200

    Adding Button, closes #11

---
+++ bootstrap/src/main/java/net/dontdrinkandroot/wicket/bootstrap/component/button/Button.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2012-2016 Philip Washington Sorst <philip@sorst.net>
+ * and individual contributors as indicated
+ * by the @authors tag.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.dontdrinkandroot.wicket.bootstrap.component.button;
+
+import net.dontdrinkandroot.wicket.bootstrap.behavior.ButtonBehavior;
+import net.dontdrinkandroot.wicket.bootstrap.css.ButtonSize;
+import net.dontdrinkandroot.wicket.bootstrap.css.ButtonStyle;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.model.IModel;
+
+public abstract class Button<T> extends Link<T> implements IButton
+{
+	protected ButtonBehavior buttonBehavior = new ButtonBehavior();
+
+	public Button(String id)
+	{
+		super(id);
+	}
+
+	public Button(String id, IModel<T> model)
+	{
+		super(id, model);
+	}
+
+	@Override
+	protected void onInitialize()
+	{
+		super.onInitialize();
+		this.add(this.buttonBehavior);
+	}
+
+	@Override
+	public ButtonSize getButtonSize()
+	{
+		return this.buttonBehavior.getButtonSize();
+	}
+
+	@Override
+	public Button<T> setButtonSize(ButtonSize buttonSize)
+	{
+		this.buttonBehavior.setButtonSize(buttonSize);
+		return this;
+	}
+
+	@Override
+	public ButtonStyle getButtonStyle()
+	{
+		return this.buttonBehavior.getButtonStyle();
+	}
+
+	@Override
+	public Button<T> setButtonStyle(ButtonStyle buttonStyle)
+	{
+		this.buttonBehavior.setButtonStyle(buttonStyle);
+		return this;
+	}
+
+	public Button<T> setButtonSizeModel(IModel<ButtonSize> buttonSizeModel)
+	{
+		this.buttonBehavior.setButtonSizeModel(buttonSizeModel);
+		return this;
+	}
+
+	public Button<T> setButtonStyleModel(IModel<ButtonStyle> buttonStyleModel)
+	{
+		this.buttonBehavior.setButtonStyleModel(buttonStyleModel);
+		return this;
+	}
+}
