9094500dedc330e7f8331ed33f5182b6e6ab2870
Philip Washington Sorst <philip@sorst.net>
2016-09-11 22:14:11 +0200

    Moving SimpleForm to extras

--- extras/src/main/java/net/dontdrinkandroot/wicket/extras/page/component/form/SimpleAjaxFormPanel.java
+++ extras/src/main/java/net/dontdrinkandroot/wicket/extras/page/component/form/SimpleAjaxFormPanel.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.dontdrinkandroot.wicket.bootstrap.component.form;
+package net.dontdrinkandroot.wicket.extras.page.component.form;
 
 import net.dontdrinkandroot.wicket.bootstrap.behavior.PanelBehavior;
 import net.dontdrinkandroot.wicket.component.basic.Heading;
--- extras/src/main/java/net/dontdrinkandroot/wicket/extras/page/component/form/SimpleForm.java
+++ extras/src/main/java/net/dontdrinkandroot/wicket/extras/page/component/form/SimpleForm.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.dontdrinkandroot.wicket.bootstrap.component.form;
+package net.dontdrinkandroot.wicket.extras.page.component.form;
 
 import net.dontdrinkandroot.wicket.bootstrap.behavior.form.FormStyleBehavior;
 import net.dontdrinkandroot.wicket.bootstrap.component.feedback.FencedFeedbackPanel;
