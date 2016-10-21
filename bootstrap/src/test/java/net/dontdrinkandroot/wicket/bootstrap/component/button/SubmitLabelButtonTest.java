9bcb0a5ee2c2d9b3b87cfb91da25f28a69e6a192
Philip Washington Sorst <philip@sorst.net>
2016-10-21 13:37:00 +0200

    Adding SubmitLabelButton Test, refs #18

---
+++ bootstrap/src/test/java/net/dontdrinkandroot/wicket/bootstrap/component/button/SubmitLabelButtonTest.java
@@ -0,0 +1,25 @@
+package net.dontdrinkandroot.wicket.bootstrap.component.button;
+
+import net.dontdrinkandroot.wicket.bootstrap.AbstractWicketTest;
+import org.apache.wicket.core.util.string.ComponentRenderer;
+import org.apache.wicket.util.tester.TagTester;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Philip Washington Sorst <philip@sorst.net>
+ */
+public class SubmitLabelButtonTest extends AbstractWicketTest
+{
+    @Test
+    public void testDefaultMarkup()
+    {
+        SubmitLabelButton component = new SubmitLabelButton("id");
+        String componentMarkup = ComponentRenderer.renderComponent(component).toString();
+
+        TagTester componentTester = TagTester.createTagByAttribute(componentMarkup, "wicket:id", "id");
+        Assert.assertTrue(componentTester.getAttributeIs("type", "submit"));
+        Assert.assertTrue(componentTester.getAttributeIs("class", "btn btn-primary"));
+        Assert.assertEquals("button", componentTester.getName());
+    }
+}
