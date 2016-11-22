72f2a4d61594f8e2e42ac36c85c16c082deff451
Philip Washington Sorst <philip@sorst.net>
2016-11-22 13:37:00 +0100

    Adding CssClassModifierTest, refs# 18

---
+++ core/src/test/java/net/dontdrinkandroot/wicket/behavior/CssClassModifierTest.java
@@ -0,0 +1,66 @@
+package net.dontdrinkandroot.wicket.behavior;
+
+import net.dontdrinkandroot.wicket.AbstractWicketTest;
+import net.dontdrinkandroot.wicket.css.CssClass;
+import net.dontdrinkandroot.wicket.css.SimpleCssClass;
+import org.apache.wicket.core.util.string.ComponentRenderer;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Philip Washington Sorst <philip@sorst.net>
+ */
+public class CssClassModifierTest extends AbstractWicketTest
+{
+    @Test
+    public void testStringConstructor()
+    {
+        WebMarkupContainer container = new WebMarkupContainer("id");
+        container.add(new CssClassModifier("testone"));
+        CharSequence componentMarkup = ComponentRenderer.renderComponent(container);
+
+        Assert.assertEquals(
+                "<wicket:container wicket:id=\"id\" class=\"testone\"></wicket:container>",
+                componentMarkup.toString()
+        );
+    }
+
+    @Test
+    public void testCssClassConstructor()
+    {
+        WebMarkupContainer container = new WebMarkupContainer("id");
+        container.add(new CssClassModifier(new SimpleCssClass("testone")));
+        CharSequence componentMarkup = ComponentRenderer.renderComponent(container);
+
+        Assert.assertEquals(
+                "<wicket:container wicket:id=\"id\" class=\"testone\"></wicket:container>",
+                componentMarkup.toString()
+        );
+    }
+
+    @Test
+    public void testModelConstructor()
+    {
+        IModel<CssClass> classModel = Model.of(new SimpleCssClass("testone"));
+        WebMarkupContainer container = new WebMarkupContainer("id");
+        container.add(new CssClassModifier(classModel));
+        CharSequence componentMarkup = ComponentRenderer.renderComponent(container);
+
+        Assert.assertEquals(
+                "<wicket:container wicket:id=\"id\" class=\"testone\"></wicket:container>",
+                componentMarkup.toString()
+        );
+
+        classModel.setObject(new SimpleCssClass("testtwo"));
+
+        componentMarkup = ComponentRenderer.renderComponent(container);
+
+        Assert.assertEquals(
+                "<wicket:container wicket:id=\"id\" class=\"testtwo\"></wicket:container>",
+                componentMarkup.toString()
+        );
+    }
+}
