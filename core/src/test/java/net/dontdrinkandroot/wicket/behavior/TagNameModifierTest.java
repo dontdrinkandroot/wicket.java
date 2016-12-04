8faf64e881beac647ef271919f55dcc3d1310d41
Philip Washington Sorst <philip@sorst.net>
2016-12-04 13:37:00 +0100

    Adding TagNameModifierTest

---
+++ core/src/test/java/net/dontdrinkandroot/wicket/behavior/TagNameModifierTest.java
@@ -0,0 +1,26 @@
+package net.dontdrinkandroot.wicket.behavior;
+
+import net.dontdrinkandroot.wicket.AbstractWicketTest;
+import org.apache.wicket.core.util.string.ComponentRenderer;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Philip Washington Sorst <philip@sorst.net>
+ */
+public class TagNameModifierTest extends AbstractWicketTest
+{
+    @Test
+    public void testModelConstructor()
+    {
+        WebMarkupContainer container = new WebMarkupContainer("id");
+        container.add(new TagNameModifier("div"));
+        CharSequence componentMarkup = ComponentRenderer.renderComponent(container);
+
+        Assert.assertEquals(
+                "<wicket:div wicket:id=\"id\"></wicket:div>",
+                componentMarkup.toString()
+        );
+    }
+}
