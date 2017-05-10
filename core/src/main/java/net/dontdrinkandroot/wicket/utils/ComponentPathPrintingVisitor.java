c81f013fb2080effadaf9e709fb2fb7abf7bda9b
Philip Washington Sorst <philip@sorst.net>
2017-05-10 13:37:00 +0200

    Adding ComponentPathPrintingVisitor

---
+++ core/src/main/java/net/dontdrinkandroot/wicket/utils/ComponentPathPrintingVisitor.java
@@ -0,0 +1,19 @@
+package net.dontdrinkandroot.wicket.utils;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.util.visit.IVisit;
+import org.apache.wicket.util.visit.IVisitor;
+
+/**
+ * Prints all component paths and their classes.
+ *
+ * @author Philip Washington Sorst <philip@sorst.net>
+ */
+public class ComponentPathPrintingVisitor implements IVisitor<Component, Void>
+{
+    @Override
+    public void component(Component component, IVisit<Void> visit)
+    {
+        System.out.println(String.format("%s: %s", component.getPageRelativePath(), component.getClass()));
+    }
+}
