bcd74dd69164d56334486c7947f1c224427954bc
Philip Washington Sorst <philip@sorst.net>
2018-06-22 17:44:51 +0200

    Spring boot to 2.x

--- example/src/main/java/net/dontdrinkandroot/wicket/example/Application.java
+++ example/src/main/java/net/dontdrinkandroot/wicket/example/Application.java
@@ -20,13 +20,12 @@ package net.dontdrinkandroot.wicket.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
 
 /**
  * @author Philip Washington Sorst <philip@sorst.net>
  */
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
+@EnableAutoConfiguration
 public class Application
 {
     public static void main(String args[])
