df3311838c6a58dad7b50abbb96121845933a492
Philip Washington Sorst <philip@sorst.net>
2018-06-26 16:09:43 +0200

    Removing Spring MVC Error configuration

--- example/src/main/java/net/dontdrinkandroot/wicket/example/Application.java
+++ example/src/main/java/net/dontdrinkandroot/wicket/example/Application.java
@@ -20,12 +20,13 @@ package net.dontdrinkandroot.wicket.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
 /**
  * @author Philip Washington Sorst <philip@sorst.net>
  */
 @SpringBootApplication
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 public class Application
 {
     public static void main(String args[])
