df4bce716e6811fd12e40e2fcf79d03074b03675
Philip Washington Sorst <philip@sorst.net>
2021-04-05 17:54:35 +0200

    Adding updateAjaxAttributes handler

--- core/src/main/kotlin/net/dontdrinkandroot/wicket/behavior/ajax/AjaxFormComponentUpdatingBehavior.kt
+++ core/src/main/kotlin/net/dontdrinkandroot/wicket/behavior/ajax/AjaxFormComponentUpdatingBehavior.kt
@@ -1,13 +1,24 @@
 package net.dontdrinkandroot.wicket.behavior.ajax
 
 import org.apache.wicket.ajax.AjaxRequestTarget
+import org.apache.wicket.ajax.attributes.AjaxRequestAttributes
 import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior
+import org.apache.wicket.util.lang.Checks
 
 inline fun ajaxUpdate(
     event: String,
-    crossinline onUpdateHandler: AjaxFormComponentUpdatingBehavior.(target: AjaxRequestTarget) -> Any?
+    crossinline updateAjaxAttributesHandler: AjaxRequestAttributes.(behavior: AjaxFormComponentUpdatingBehavior) -> Unit,
+    crossinline onUpdateHandler: AjaxFormComponentUpdatingBehavior.(target: AjaxRequestTarget) -> Unit
 ): AjaxFormComponentUpdatingBehavior = object : AjaxFormComponentUpdatingBehavior(event) {
     override fun onUpdate(target: AjaxRequestTarget) {
         onUpdateHandler(this, target)
+    }
+
+    override fun updateAjaxAttributes(attributes: AjaxRequestAttributes) {
+        val evt = this.event
+        Checks.notEmpty(evt, "getEvent() should return non-empty event name(s)", *arrayOfNulls(0))
+        attributes.setEventNames(*arrayOf(evt))
+        attributes.method = AjaxRequestAttributes.Method.POST
+        updateAjaxAttributesHandler(attributes, this)
     }
 }
